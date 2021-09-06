09ba7fdfceef60625643bb63f01d6f57e95b96a8
朱子涵 <928240390@qq.com>
2021-09-06 21:35:13 +0800

    完成了p07_p07_encrypt_decrypt

---
+++ level1/p07_encrypt_decrypt/p07_decrypt.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+using namespace std;
+const int N = 101;
+
+unsigned int seed;
+char s[101], enc[256], dec[256];
+int val[256];
+
+bool cmp(char a, char b){
+	return val[a] < val[b];
+}
+
+void init(){
+	for(int i = 32; i <= 126; i++)
+		enc[i] = i, val[i] = rand();
+	sort(enc + 32, enc + 127, cmp);
+	for(int i = 32; i <= 126; i++)
+		dec[enc[i]] = i;
+}
+
+int main(){
+	while(true){
+		printf("请输入加密种子（unsigned int）：");
+		scanf("%u", &seed);
+		srand(seed);
+		init();
+		char c = getchar();
+		while(c != '\n') c = getchar();
+		printf("请输入字符串（最大长度100，请注意，支持空格）：");
+		gets(s);
+		for(char *c = s; *c; c++)
+			putchar(dec[*c]);
+		puts("[结束]");
+	}
+	return 0;
+}
