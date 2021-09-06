90440838270cb6a0885a38f21ccdcf4e5ec4d314
朱子涵 <928240390@qq.com>
2021-09-06 21:43:19 +0800

    完成了p08_hanoi

---
+++ level1/p08_hanoi/p08_hanoi.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <windows.h>
+using namespace std;
+const int N = 64;
+
+void f(int n, char from, char to, char pass){
+	if(!n) return;
+	f(n - 1, from, pass, to);
+	printf("%c -> %c\n", from, to);
+	f(n - 1, pass, to, from);
+}
+
+int main(){
+	f(N, 'A', 'B', 'C');
+	printf("%d²ãººÅµËþÒÆ¶¯Íê±Ï\n", N);
+	system("pause");
+	return 0;
+}
