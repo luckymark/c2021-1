077c80711720f59ef71bd20707167f6a25cdf7db
朱子涵 <928240390@qq.com>
2021-09-06 23:42:02 +0800

    重写p01_runningLetter，p05_allPrimes补上时间计算

--- level1/p01_runningLetter/p01_runningLetter.cpp
+++ level1/p01_runningLetter/p01_runningLetter.cpp
@@ -1,32 +1,30 @@
 #include <cstdio>
 #include <windows.h>
 using namespace std;
-const int N = 80, M = 10, sleeptime = 10;
+const int N = 80, M = 10, sleeptime = 1;
 
 char c;
+
+void printtimes(char c, int t){
+	while(t--) putchar(c);
+}
+
+void print(int i){
+	system("cls");
+	printtimes('\n', M);
+	printtimes(' ', i);
+	putchar(c);
+	Sleep(sleeptime);
+}
 
 int main(){
 	printf("ÇëÊäÈë×Ö·û£º");
 	scanf(" %c", &c);
 	while(true){
-		for(int i = 0; i < N; i++){
-			system("cls");
-			for(int j = 0; j < M; j++)
-				putchar('\n');
-			for(int j = 0; j < i; j++)
-				putchar(' ');
-			putchar(c);
-			Sleep(sleeptime);
-		}
-		for(int i = N - 1; ~i; i--){
-			system("cls");
-			for(int j = 0; j < M; j++)
-				putchar('\n');
-			for(int j = 0; j < i; j++)
-				putchar(' ');
-			putchar(c);
-			Sleep(sleeptime);
-		}
+		for(int i = 0; i < N; i++)
+			print(i);
+		for(int i = N - 1; ~i; i--)
+			print(i);
 	}
 	return 0;
 }
--- level1/p05_allPrimes/p05_allPrimes.cpp
+++ level1/p05_allPrimes/p05_allPrimes.cpp
@@ -8,17 +8,24 @@ int prime[N], len;
 bool vis[N];
 
 int main(){
+	LARGE_INTEGER li;
+	QueryPerformanceFrequency(&li);
+	double freq = li.QuadPart;
+	QueryPerformanceCounter(&li);
+	double start = li.QuadPart;
 	for(int i = 2; i < N; i++){
-		if(!vis[i]){
-			printf("%d\n", i);
-			prime[++len] = i;
-		}
+		if(!vis[i]) prime[++len] = i;
 		for(int j = 1; j <= len && i * prime[j] < N; j++){
 			vis[i * prime[j]] = true;
 			if(i % prime[j] == 0) break;
 		}
 	}
-	puts("1~1000内质数打印完成");
+	QueryPerformanceCounter(&li);
+	double end = li.QuadPart;
+	printf("1~1000内质数计算完成，所用时间：%.07lf s\n接下来开始打印素数\n", (end - start) / freq);
+	for(int i = 1; i <= len; i++)
+		printf("%d\n", prime[i]);
+	puts("1~1000以内素数打印完毕");
 	system("pause");
 	return 0;
 }
