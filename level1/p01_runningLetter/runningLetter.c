9bff86b04ce8ac90159ff1d1f511140a42f6ddf7
Kningc <1049052757@qq.com>
2021-09-09 00:20:48 +0800

    完成了running letter

---
+++ level1/p01_runningLetter/runningLetter.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+
+
+int main()
+{
+	int speed = 0;
+    printf("Please input a speed.(1-1000)\n");
+    if(!(scanf("%d",&speed) && speed>0 && speed<1001))
+    {
+        printf("goodbye!");
+        exit(1);
+    }
+
+
+
+    int sleepSpeed = 1000/speed;
+    for (int i = 0; i <= 80; i++)
+	{
+
+		printf("w\b\b ");
+
+		Sleep(sleepSpeed);
+		printf("w");
+		Sleep(sleepSpeed);
+	}
+	for (int i = 0; i <= 80; i++)
+	{
+		Sleep(sleepSpeed*2);
+		printf("\b \b\bw");
+	}
+
+	return 0;
+
+}
