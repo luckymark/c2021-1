8b0a05e19f10846f27e88e66edf2b8c9bf27803d
justcyl <chenyilin0760@gmail.com>
2021-09-08 21:54:16 +0800

    update running letter

--- level1/p01_runningLetter/p01.cpp
+++ level1/p01_runningLetter/p01.cpp
@@ -5,17 +5,17 @@
 
 using namespace std;
 
-const int halt = 80;
+const int turn_dir = 100;
 int main(){
 	int n = 1;
 	int c = 1;
 	while(1){
 		
 		for (int j = 1; j <= abs(n); j++) printf(" ");
-		printf("1");
+		printf("%c", c == 1 ? '>':'<');
 		system("cls");
 		n += c;
-		if (abs(n) == halt) c = -c;
+		if (abs(n) == turn_dir) c = -c;
 	}
 	return 0;
 }
