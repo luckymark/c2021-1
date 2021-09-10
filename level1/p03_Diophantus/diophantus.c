3b907c7d1f1dd822fda118b4e3cd113fd48762cf
LostKningc <61621829+LostKningc@users.noreply.github.com>
2021-09-10 18:57:58 +0800

    完成了Diophantus

---
+++ level1/p03_Diophantus/diophantus.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int equal(float,float);
+int main()
+{
+    for(int age=10;age <= 200 ;age++)
+    {
+        if(equal(age/6.0+age/12.0+age/7.0+5+age/2+4,age))
+            printf("Diophantus may be %d years old when he died. ",age);
+    }
+    return 0;
+}
+
+int equal(float a ,float b)
+{
+    if(a>=b)
+    {
+        if(a-b<1.0e-6)
+            return 1;
+        else
+            return 0;
+    }
+    else
+    {
+        if(b-a<1.0e-6)
+            return 1;
+        else
+            return 0;
+    }
+}
+
