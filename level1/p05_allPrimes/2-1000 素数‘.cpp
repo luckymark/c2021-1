12165ba23c6ff2f49f9b43d6411bd43bc527ad86
dxl <617267707@qq.com>
2021-09-17 19:08:23 +0800

    finish allprimes

---
+++ level1/p05_allPrimes/2-1000 素数‘.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string.h>
+#include<cmath>
+#include<time.h> 
+using namespace std;
+int a[1001];
+int main()
+	{	
+		int j;
+		double begin,end;
+		int isprime(int x);
+		begin=clock();
+		for(int i=2;i<=1000;++i)
+			{
+				a[i]=1;
+			}
+		for(int i=2;i<=1000;++i)
+			{	
+				if (a[i]==1){	
+					if (isprime(i))
+						{
+							cout<<i<<"  ";
+							j=i*2;
+							while(j<=1000){																																				
+								a[j]=0;
+								j=j+i;
+							}
+						}	
+				}
+			}
+		end=clock();
+		cout<<endl;
+		cout<<"ำรมห"<<(double)((end-begin)/1000)<<"S"<<endl;
+		return 0;
+	}
+int isprime(int x)
+	{	
+		int flag=1;
+		for(int i=2;i<=(int)(sqrt(x));++i)
+			{	
+				if(x%i==0)	flag=0;
+			}
+		return flag;
+	}
