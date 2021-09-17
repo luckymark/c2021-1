dda2d9e019bae070ddc0c13c0e2f3f2b0dd7be0a
yuanbanlin201 <2417439457@qq.com>
2021-09-17 17:06:25 +0800

    finish 01-06

---
+++ level1/p06_Goldbach/cmake-build-debug/goldbach/CMakeFiles/goldbach.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/myprojects/c2021-1/level1/p06_Goldbach/goldbach/goldbach.c" "D:/myprojects/c2021-1/level1/p06_Goldbach/cmake-build-debug/goldbach/CMakeFiles/goldbach.dir/goldbach.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ level1/p06_Goldbach/cmake-build-debug/goldbach/CMakeFiles/goldbach.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/goldbach.dir/goldbach.c.obj"
+  "goldbach.exe"
+  "goldbach.exe.manifest"
+  "goldbach.pdb"
+  "libgoldbach.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/goldbach.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
