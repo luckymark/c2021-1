dfe88039a49598103d4ca3507ac3a864744a6edf
sanaprix <kyru_yann@qq.com>
2021-09-27 13:13:05 +0800

    Configured GPG Keys

---
+++ level1/p09_maze/Maze.cpp
@@ -0,0 +1,28 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+
+#define INF 0x7f7f7f7f
+
+#define max(x,y) (x)>(y)?(x):(y)
+#define min(x,y) (x)<(y)?(x):(y)
+#define swap(x,y) {int T=x; x=y; y=T;}
+#define wipe(x,y) memset(x,y,sizeof(x))
+#define rep(x,y,z) for(int x=y,I=z;x<=I;++x)
+
+#define dbgIn(x) freopen(x".in","r+",stdin)
+#define dbgOut(x) freopen(x".out","w+",stdout)
+
+typedef long long LL;
+typedef unsigned long long ULL;
+
+inline void Read(int &x){
+	x=0; char ch=getchar();
+	while(ch>='0' && ch<='9') x=x*10+ch-'0',ch=getchar();
+}
+
+int main(){
+	return 0;
+}
+
