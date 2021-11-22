99a8ad87c7f45b53d973680ce269b90aeebfbc1e
chunshu10 <90622131+chunshu10@users.noreply.github.com>
2021-11-22 23:12:34 +0800

    Add files via upload

---
+++ level1/p08_hanoi/题8.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+	int n;
+	int d[3]={'a','b','c'};
+	scanf ("%d",&n);
+	g(n,d[0],d[1],d[2]);
+	return 0;}
+	
+	g(int h,int l,int m,int o){
+		if(h==1){
+			printf ("%c-->%c ",l,m);
+			}else{
+				g(h-1,l,m,o);
+				printf ("%c-->%c ",l,o);
+				g(h-1,m,o,l);
+			}
+
+} 
