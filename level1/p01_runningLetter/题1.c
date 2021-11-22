f5000785e874b06e64870e0fecaa8e9449710bc3
chunshu10 <90622131+chunshu10@users.noreply.github.com>
2021-11-22 22:51:35 +0800

    Add files via upload

--- level1/p01_runningLetter/题1.c
+++ level1/p01_runningLetter/题1.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
-int main()
-{int n;
-int i;
-int a;
-int b;
-for(n=1;n<120;n++){
-for(i=1;i<n;i++){
-printf(" ");
-}
-printf("A");
-Sleep(20);
-system("cls");}
-for(a=120;a>1;a--){
-for(b=1;b<a;b++){
-printf(" ");
-}printf("A");
-Sleep(20);
-system("cls");
-}
-return ;
+int main(){
+	int n;
+	int i;
+	int a;
+	int b;
+	for(n=1;n<120;n++){
+	for(i=1;i<n;i++){
+		printf(" ");
+	}
+	printf("A");
+	Sleep(20);
+	system("cls");}
+	for(a=120;a>1;a--){
+		for(b=1;b<a;b++){
+			printf(" ");
+	}printf("A");
+	Sleep(20);
+	system("cls");
+	}
+	return ;
 }
