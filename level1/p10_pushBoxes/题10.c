53e6ffa7b6185c0cb9fcd0656d8b008334cb8bc1
chunshu10 <90622131+chunshu10@users.noreply.github.com>
2021-12-13 00:00:28 +0800

    Add files via upload

---
+++ level1/p10_pushBoxes/题10.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+	char ch;
+	int i,j;
+	int m=1;
+	int n=1;
+char map[8][9]={"########\n",
+                "#o     #\n",
+				"# @  @ #\n", 
+				"#  *   #\n",
+				"#      #\n",
+				"#      #\n",
+				"#     *#\n",
+				"########\n"};
+printf("%s",map);
+while((ch = getch())!= 0x1B){
+	if(map[6][6]=='@'&&map[3][3]=='@'){system("cls");
+printf("YOU WIN");
+exit(1);}
+switch(ch = getch()){
+case 72:system("cls") ;
+for(i=0;i<=8;i++)
+{for(j=0;j<=8;j++)
+{if(map[i][j]=='#'){map[i][j]=='#';
+}else if(map[i][j]=='*'){map[i][j]='*';
+}
+else if(map[i][j]=='@'){map[i][j]='@'; 
+}
+else if(map[i][j]==' '){map[i][j]==' ';
+}else if(map[i][j]=='o'){if(map[i-1][j]==' ')	
+{map[i][j]=' ';
+map[i-1][j]='o';
+}
+else if(map[i-1][j]=='@'){if(map[i-2][j]==' '||map[i-2][j]=='*'){
+	map[i][j]=' ';
+	map[i-1][j]='o';
+	map[i-2][j]='@';
+}
+}
+}else if(map[i][j]=='\n'){map[i][j]=='\n';
+}
+}
+}printf("%s",map);
+break;
+case 80:system("cls") ;
+m=1;
+for(i=0;i<=8;i++)
+{for(j=0;j<=8;j++)
+{if(map[i][j]=='#'){map[i][j]=='#';
+}else if(map[i][j]=='*'){map[i][j]='*';
+}
+else if(map[i][j]==' '){map[i][j]==' ';
+}else if(map[i][j]=='o'&&m==1){if(map[i+1][j]==' ')	
+{map[i][j]=' ';
+map[i+1][j]='o';
+m=0;
+}else if(map[i+1][j]=='@'){if(map[i+2][j]==' '||map[i+2][j]=='*'){
+	map[i][j]=' ';
+	map[i+1][j]='o';
+	map[i+2][j]='@';
+	m=0;
+}
+}
+}else if(map[i][j]=='\n'){map[i][j]=='\n';
+}
+}
+}printf("%s",map);break;
+case 75:system("cls") ;
+for(i=0;i<=8;i++)
+{for(j=0;j<=8;j++)
+{if(map[i][j]=='#'){map[i][j]=='#';
+}else if(map[i][j]=='*'){map[i][j]='*';
+}
+else if(map[i][j]==' '){map[i][j]==' ';
+}else if(map[i][j]=='o'){if(map[i][j-1]==' ')	
+{map[i][j]=' ';
+map[i][j-1]='o';
+}else if(map[i][j-1]=='@'){if(map[i][j-2]==' '||map[i][j-2]=='*'){
+	map[i][j]=' ';
+	map[i][j-1]='o';
+	map[i][j-2]='@';
+}
+}
+}else if(map[i][j]=='\n'){map[i][j]=='\n';
+}
+}
+}printf("%s",map);break;
+case 77:system("cls") ;
+n=1;
+for(i=0;i<=8;i++)
+{ for(j=0;j<=8;j++)
+{if(map[i][j]=='#'){map[i][j]=='#';
+}else if(map[i][j]=='*'){map[i][j]='*';
+}
+else if(map[i][j]==' '){map[i][j]==' ';
+}else if(map[i][j]=='o'&&n==1){if(map[i][j+1]==' ')	
+{map[i][j]=' ';
+map[i][j+1]='o';
+n=0;
+}else if(map[i][j+1]=='@'){if(map[i][j+2]==' '||map[i][j+2]=='*'){
+	map[i][j]=' ';
+	map[i][j+1]='o';
+	map[i][j+2]='@';
+	n=0;
+}
+}
+}else if(map[i][j]=='\n'){map[i][j]=='\n';
+}
+}
+}printf("%s",map);break;
+default : break;
+}
+}return 0;
+}
