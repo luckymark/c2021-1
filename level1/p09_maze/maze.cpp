010b5969500a69ee17e44d70b498f34238d640eb
apprentice4 <2835494461@qq.com>
2021-12-15 16:31:23 +0800

    1-11胡纯哲

---
+++ level1/p09_maze/maze.cpp
@@ -0,0 +1,263 @@
+﻿#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h> 
+#define Height 25 
+#define Width 25 
+#define Wall 1
+#define Road 0
+#define Start 2
+#define End 3
+#define Esc 5
+#define Up 1
+#define Down 2
+#define Left 3
+#define Right 4
+#define Auto  6
+int map[Height + 2][Width + 2];
+int mask[Height + 2][Width + 2];
+int RecordX[(Height + 2) * (Width + 2) + 10];
+int RecordY[(Height + 2) * (Width + 2) + 10];
+int RecordFX[(Height + 2) * (Width + 2) + 10];
+int StepX[4];
+int StepY[4];
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+void hidden()
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	GetConsoleCursorInfo(hOut, &cci);
+	cci.bVisible = 0;
+	SetConsoleCursorInfo(hOut, &cci);
+}
+void create(int x, int y)
+{
+	int c[4][2] = { 0,1,1,0,0,-1,-1,0 };
+	int i, j, t;
+	for (i = 0; i < 4; i++)
+	{
+		j = rand() % 4;
+		t = c[i][0]; c[i][0] = c[j][0]; c[j][0] = t;
+		t = c[i][1]; c[i][1] = c[j][1]; c[j][1] = t;
+	}
+	map[x][y] = Road;
+	for (i = 0; i < 4; i++)
+		if (map[x + 2 * c[i][0]][y + 2 * c[i][1]] == Wall)
+		{
+			map[x + c[i][0]][y + c[i][1]] = Road;
+			create(x + 2 * c[i][0], y + 2 * c[i][1]);
+		}
+}
+int get_key()
+{
+	char c;
+	while (c = _getch())
+	{
+		if (c == ' ') return Auto;
+		if (c == 27) return Esc;
+		if (c != -32)continue;
+		c = _getch();
+		if (c == 72) return Up;
+		if (c == 80) return Down;
+		if (c == 75) return Left;
+		if (c == 77) return Right;
+	}
+	return 0;
+}
+void paint(int x, int y)
+{
+	gotoxy(2 * y - 2, x - 1);
+	switch (map[x][y])
+	{
+	case Start:
+		printf("入"); break;
+	case End:
+		printf("出"); break;
+	case Wall:
+		printf("▇"); break;
+	case Road:
+		printf("  "); break;
+	}
+}
+void game()
+{
+	int x = 2, y = 1;
+	int c;
+	int  curr_step = 0;
+	int failflag = 0;
+	StepX[0] = 0;
+	StepY[0] = 1;
+	StepX[2] = 0;
+	StepY[2] = -1;
+	StepX[1] = -1;
+	StepY[1] = 0;
+	StepX[3] = 1;
+	StepY[3] = 0;
+	for (int i = 0; i < Height + 2; i++)
+		for (int j = 0; j < Width + 2; j++)
+			mask[i][j] = 0;
+	for (int i = 0; i < (Height + 2) * (Width + 2) + 10; i++)
+	{
+		RecordX[i] = 0;
+		RecordY[i] = 0;
+		RecordFX[i] = 0;
+	}
+	RecordX[0] = x;
+	RecordY[0] = y;
+	RecordFX[0] = 0;
+	mask[x][y] = 1;
+	c = 0;
+	while (failflag == 0)
+	{
+		gotoxy(2 * y - 2, x - 1);
+		printf("●");
+		if (map[x][y] == End)
+		{
+			gotoxy(30, 25);
+			printf("恭喜通关");
+			break;
+		}
+		if (c != Auto)
+			c = get_key();
+		else Sleep(100);
+		if (c == Esc)
+		{
+			gotoxy(0, 24);
+			break;
+		}
+		switch (c)
+		{
+		case Up:
+			if (map[x - 1][y] != Wall)
+			{
+				paint(x, y);
+				x--;
+				curr_step++;
+				RecordX[curr_step] = x;
+				RecordY[curr_step] = y;
+				RecordFX[curr_step] = 0;
+			}
+			break;
+		case Down:
+			if (map[x + 1][y] != Wall)
+			{
+				paint(x, y);
+				x++;
+				curr_step++;
+				RecordX[curr_step] = x;
+				RecordY[curr_step] = y;
+				RecordFX[curr_step] = 0;
+			}
+			break;
+		case Left:
+			if (map[x][y - 1] != Wall)
+			{
+				paint(x, y);
+				y--;
+				curr_step++;
+				RecordX[curr_step] = x;
+				RecordY[curr_step] = y;
+				RecordFX[curr_step] = 0;
+			}
+			break;
+		case Right:
+			if (map[x][y + 1] != Wall)
+			{
+				paint(x, y);
+				y++;
+				curr_step++;
+				RecordX[curr_step] = x;
+				RecordY[curr_step] = y;
+				RecordFX[curr_step] = 0;
+			}
+			break;
+		case Auto:
+			if (1)
+			{
+				int mflag = 0;
+				for (int m = RecordFX[curr_step]; m < 4; m++)
+				{
+					int x1, y1;
+					x1 = x + StepX[m];
+					y1 = y + StepY[m];
+					if (map[x1][y1] != Wall && mask[x1][y1] == 0)
+					{
+						paint(x, y);
+						x = x1;
+						y = y1;
+						RecordFX[curr_step] = m;
+						curr_step++;
+						RecordX[curr_step] = x;
+						RecordY[curr_step] = y;
+						mask[x][y] = 1;
+						RecordFX[curr_step] = 0;
+						mflag = 1;
+						break;
+					}
+					else RecordFX[curr_step] = m;
+				}
+				if (mflag == 0)
+				{
+					while (curr_step >= 0 && RecordFX[curr_step] == 3)
+					{
+						paint(x, y);
+						mask[x][y] = 0;
+						curr_step = curr_step - 1;
+						x = RecordX[curr_step];
+						y = RecordY[curr_step];
+					}
+					if (curr_step >= 0)
+					{
+						paint(x, y);
+						mask[x][y] = 0;
+						curr_step = curr_step - 1;
+						x = RecordX[curr_step];
+						y = RecordY[curr_step];
+
+						RecordFX[curr_step] = RecordFX[curr_step] + 1;
+					}
+
+
+				}
+			}
+			break;
+		}
+	}
+}
+int main()
+{
+	int i, j;
+	srand(time(NULL));
+	hidden();
+	for (i = 0; i <= Height + 1; i++)
+		for (j = 0; j <= Width + 1; j++)
+			if (i == 0 || i == Height + 1 || j == 0 || j == Width + 1)
+				map[i][j] = Road;
+			else map[i][j] = Wall;
+
+	create(2 * (rand() % (Height / 2) + 1), 2 * (rand() % (Width / 2) + 1));
+	for (i = 0; i <= Height + 1; i++)
+	{
+		map[i][0] = Wall;
+		map[i][Width + 1] = Wall;
+	}
+	for (j = 0; j <= Width + 1; j++)
+	{
+		map[0][j] = Wall;
+		map[Height + 1][j] = Wall;
+	}
+	map[2][1] = Start;
+	map[Height - 1][Width] = End;
+	for (i = 1; i <= Height; i++)
+		for (j = 1; j <= Width; j++)
+			paint(i, j);
+	game();
+	return 0;
+}
---
+++ level1/p10_pushBoxes/pushbox.cpp
@@ -0,0 +1,247 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+int WIDTH;
+int HEIGHT;
+// 关卡
+int checkpoint = 1;
+/**
+ *	0	表示空
+ *	1	表示墙
+ *	2	表示人
+ *	3	表示箱子
+ *	4	表示球（箱子需要推到的位置）
+ *	5	表示推到目的地的箱子
+ */
+int map[10][10];
+
+//人的位置
+int x, y;
+
+//箱子的个数
+int boxs;
+
+//声明方法
+void createMap();
+void initData();
+void drawMap();
+void move(char direction);
+
+int main(int argc, char* argv[])
+{
+    char direction; //存储键盘按的方向
+    createMap();
+    initData();
+    // while (1);
+    while (1)
+    {
+        system("cls");
+        drawMap();
+
+        if (!boxs)
+        {
+            if (checkpoint == 3)
+            {
+                break;
+            }
+            else
+            {
+                checkpoint++;
+                printf("按任意键进入下一关");
+                createMap();
+                initData();
+            }
+        }
+
+        direction = getch();
+        move(direction);
+    }
+    printf("恭喜你完成游戏！※");
+    return 0;
+}
+
+/**
+ * 读取地图
+ */
+void createMap()
+{
+    FILE* fp = NULL;
+    char path[100];
+    sprintf(path, "maze%d.txt", checkpoint);
+    fp = fopen(path, "r");
+    if (checkpoint == 1)
+        WIDTH = HEIGHT = 8;
+    else if (checkpoint == 2)
+        WIDTH = HEIGHT = 9;
+    else if (checkpoint == 3)
+    {
+        WIDTH = 6;
+        HEIGHT = 8;
+    }
+    int i, j;
+    for (i = 0; i < HEIGHT; i++)
+    {
+        for (j = 0; j < WIDTH; j++)
+        {
+            map[i][j] = fgetc(fp) - '0';
+        }
+        fgetc(fp);
+    }
+}
+/**
+ *	初始化一些必须的数据
+ */
+void initData()
+{
+    int i, j;
+
+    // printf("游戏加载中，请稍后.........");
+
+    //获取人的位置
+    for (i = 0; i < HEIGHT; i++)
+    {
+        for (j = 0; j < WIDTH; j++)
+        {
+            //遍历到2时，记录人的左边
+            if (map[i][j] == 2)
+            {
+                x = j;
+                y = i;
+                map[i][j] = 0;
+            }
+            //遍历到3时，箱子的数目增加
+            if (map[i][j] == 3)
+            {
+                boxs++;
+            }
+        }
+    }
+}
+
+/**
+ *	用于在控制台显示地图
+ */
+void drawMap()
+{
+    printf("\t第《%d》关\n", checkpoint);
+    int i, j;
+    for (i = 0; i < HEIGHT; i++)
+    {
+        for (j = 0; j < WIDTH; j++)
+        {
+            if (i == y && j == x)
+            {
+                printf("♀");
+                continue;
+            }
+            switch (map[i][j])
+            {
+            case 0:
+                printf("  ");
+                break;
+            case 1:
+                printf("■");
+                break;
+            case 2:
+                // printf("♀");
+                break;
+            case 3:
+                printf("◆");
+                break;
+            case 4:
+                printf("●");
+                break;
+            case 5:
+                printf("★");
+                break;
+            }
+        }
+        printf("\n");
+    }
+}
+/**
+ *	用于移动人物
+ */
+void move(char direction)
+{
+    // 人物下一步位置
+    int next_y = y, next_x = x;
+    int dir_y = 0, dir_x = 0;
+    switch (direction)
+    {
+    case 'w':
+        dir_y--;
+        break;
+    case 'a':
+        dir_x--;
+        break;
+    case 's':
+        dir_y++;
+        break;
+    case 'd':
+        dir_x++;
+        break;
+    default:
+        return;
+    }
+    next_y += dir_y;
+    next_x += dir_x;
+
+    // 下一步越界
+    if (next_y < 0 || next_y > HEIGHT || next_x < 0 || next_x > WIDTH)
+    {
+        return;
+    }
+
+    // 下一步位置为墙
+    if (map[next_y][next_x] == 1)
+    {
+        return;
+    }
+    // 下一步位置是箱子或已完成方块
+    if (map[next_y][next_x] == 3 || map[next_y][next_x] == 5)
+    {
+        // 箱子或已完成方块的下一步越界
+        if (next_y + dir_y < 0 || next_y + dir_y > HEIGHT || next_x + dir_x < 0 || next_x + dir_x > WIDTH)
+        {
+            return;
+        }
+        // 箱子或已完成方块的下一步位置为墙或箱子
+        if (map[next_y + dir_y][next_x + dir_x] == 1 || map[next_y + dir_y][next_x + dir_x] == 3)
+        {
+            return;
+        }
+        // 箱子
+        if (map[next_y][next_x] == 3)
+        {
+            if (map[next_y + dir_y][next_x + dir_x] == 0)
+            {
+                map[next_y + dir_y][next_x + dir_x] = 3;
+                map[next_y][next_x] = 0;
+            }
+            else if (map[next_y + dir_y][next_x + dir_x] == 4)
+            {
+                map[next_y + dir_y][next_x + dir_x] = 5;
+                map[next_y][next_x] = 0;
+                boxs--;
+            }
+        }
+        // 已完成方块
+        else if (map[next_y][next_x] == 5)
+        {
+            if (map[next_y + dir_y][next_x + dir_x] == 0)
+            {
+                map[next_y + dir_y][next_x + dir_x] = 3;
+                map[next_y][next_x] = 4;
+                boxs++;
+            }
+            else if (map[next_y + dir_y][next_x + dir_x] == 4)
+            {
+                map[next_y + dir_y][next_x + dir_x] = 5;
+                map[next_y][next_x] = 4;
+            }
+        }
+    }
+    y = next_y;
+    x = next_x;
+}
