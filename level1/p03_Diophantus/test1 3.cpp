2a2f6fe7f1117bf4f41673af35da61a4f3ed5a2e
iamagithuber <90193743+iamagithuber@users.noreply.github.com>
2021-12-28 22:51:35 +0800

    Add files via upload

---
+++ level1/p03_Diophantus/test1 3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	double x;
+	int c=0;
+
+	for (x=0.0;c==0;x++){
+		double y=x/2.0;
+		double z=x/6.0+x/7.0+x/12.0+5;
+		if(z+y+4==x)
+		{ 
+			printf("¶ª·¬Í¼µÄÄêÁäÊÇ£º%f\n",x);
+			c++;
+		}
+	}
+	return 0;
+	
+}
