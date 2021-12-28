3ee23a111a166323dc7966dd57b589b92ff13d63
iamagithuber <90193743+iamagithuber@users.noreply.github.com>
2021-12-28 22:51:13 +0800

    Add files via upload

---
+++ level1/p02_isPrime/test1 2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()  
+{
+	int x;
+	int n=2;
+	int count=0;
+	scanf("%d",&x);
+	for(;n<x;n++){
+		if(x%n==0){
+		count++;
+		}
+	}
+	if(count==0){
+		printf("是素数");} 
+	else
+	printf("不是素数"); 
+	
+
+	
+		 
+	}
+
+
