480fec31152e3564b22f1880cb09c9b6e115dc5a
こくせい <90203664+Hibiyananao@users.noreply.github.com>
2022-01-03 14:09:03 +0800

    Update 02 isPrime.c

--- 作业/02 isPrime.c
+++ 作业/02 isPrime.c
@@ -3,14 +3,14 @@ int main()
 {
 	int n;
 	scanf_s("%d", &n);
-		int a = 2;
-		while (n % a != 0 && a < n)
-		{
-			a++;
-		}
-		 if (a == n)
-			printf("%d是素数",n);
-		else
-			printf("%d不是素数",n);
+	int a = 2;
+	while (n % a != 0 && a < n)
+	{
+		a++;
+	}
+	if (a == n)
+		printf("%d是素数",n);
+	else
+		printf("%d不是素数",n);
 	return 0;
 }
